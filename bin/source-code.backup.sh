a1b09dc75de7106e1ba01d9e0c0842e8e5870ad2
Terry Nightingale <tnight@pobox.com>
2016-05-09 22:17:23 -0700

    Made small tweaks to paths and switched to run for real (not dry run mode)

--- bin/source-code.backup.sh
+++ bin/source-code.backup.sh
@@ -3,16 +3,19 @@
 ########################################################################
 
 # Where we will write our log of operations 
-backupLog="~/logs/source-code.backup.log"
+backupLog="${HOME}/logs/source-code.backup.log"
+
+# Make sure the log file exists
+touch "$backupLog"
 
 # Log our start time
-echo BEGIN `date` >> $backupLog
+echo BEGIN `date` >> "$backupLog"
 
 # Save the date so we can use it to create our output directory
 backupDate=$(date "+%Y-%m-%d")
 
 # Define our source and target directories
-backupSourcePath="~/proj"
+backupSourcePath="${HOME}/proj"
 backupTargetPath="/Volumes/share/terryn/proj-backup/${backupDate}"
 
 # Make the output directory if it doesn't already exist 
@@ -22,10 +25,10 @@ mkdir -p "$backupTargetPath"
 ### /usr/bin/caffeinate -s /usr/bin/rsync -aH /Users user@rsync.net: >> ~/tmp/backup.log
 
 # Dry run mode
-/usr/bin/caffeinate -s /usr/bin/rsync --exclude=.DS_Store -Habcnv "$backupSourcePath" "$backupTargetPath" >> "$backupLog"
+### /usr/bin/caffeinate -s /usr/bin/rsync --exclude=.DS_Store -Habcnv "$backupSourcePath" "$backupTargetPath" >> "$backupLog"
 
 # The Real Deal (same as dry run mode but without -n)
-### /usr/bin/caffeinate -s /usr/bin/rsync --exclude=.DS_Store -Habcnv "$backupSourcePath" "$backupTargetPath" >> "$backupLog"
+/usr/bin/caffeinate -s /usr/bin/rsync --exclude=.DS_Store -Habcv "$backupSourcePath" "$backupTargetPath" >> "$backupLog"
 
 # Log our end time
 echo END `date` >> "$backupLog"
