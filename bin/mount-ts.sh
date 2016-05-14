d98bb6f3faa96cc1bea5a4597aaf5c5e5ac9855f
Terry Nightingale <tnight@pobox.com>
2016-05-14 13:50:53 -0700

    Login changes to mount script so I can use one script to mount TeraStation in either read-only or read-write mode.

--- bin/mount-ts.sh
+++ bin/mount-ts.sh
@@ -1,8 +1,52 @@
 #!/bin/sh
+#
+########################################################################
 
+function usage
+{
+    echo "usage: mount-ts.sh [[-r] | [-w] | [--read-only] | [--read-write] | [-h]]"
+}
+
+### Main
+readOnly=''
+readWrite=''
+
+# Get our command line arguments
+while [ "$1" != '' ]
+do
+    case $1 in
+        -r | --read-only )      shift
+                                readOnly=1
+                                ;;
+        -w | --read-write )     shift
+                                readWrite=1
+                                ;;
+        -h | --help )           usage
+                                exit 0
+                                ;;
+        * )                     usage
+                                exit 1
+    esac
+done
+
+# Either read-only or read-write mode must be specified, but not both
+if [[ ($readOnly && $readWrite) || (! $readOnly && ! $readWrite) ]]
+then
+   usage
+   exit 1
+fi
+
+# Set the mounting option
+readOnlyOption=''
+if [[ $readOnly ]]
+then
+    readOnlyOption='-r '
+fi
+
+# Mount the drive
 if mkdir /Volumes/share; then
     echo "Mounting..."
-    mount -r -t smbfs -o nodev,nosuid smb://terryn:9bum9z3n@hs-dhtgl5f5/share /Volumes/share
+    mount $readOnlyOption -t smbfs -o nodev,nosuid smb://terryn:9bum9z3n@hs-dhtgl5f5/share /Volumes/share
     echo "Mounted."
 else
     echo "NOT mounted."
