71be293c8e43af4b668f227ab8a371cb8c81281e
Terry Nightingale <tnight@pobox.com>
2016-11-26 20:56:56 -0800

    Added a "dry run" mode that prints the commands rather than running them. Also had to change from using file server's domain name to file server's IP address.

--- bin/mount-ts.sh
+++ bin/mount-ts.sh
@@ -1,29 +1,38 @@
 #!/bin/sh
 #
-# NOTE: this script requires that the user have write permission to
-# the /Volumes directory so it can create the mount point.
+# NOTE: Due to the new permissions attached to /Volumes in MacOS X
+# Sierra, this script now mounts the file share within the running
+# user's home directory and points to the file share via a symbolic
+# link in /Volumes.
 #
 ########################################################################
 
 function usage
 {
-    echo "usage: mount-ts.sh [[-r] | [-w] | [--read-only] | [--read-write] | [-h]]"
+    echo "usage: mount-ts.sh [[-r] | [-w] | [-n] | [--read-only] | [--read-write] | [--dry-run] | [-h]]"
 }
 
 ### Main
+dryRun=''
+linkTo='/Volumes/share'
+mountPoint="${HOME}/Volumes/share"
 readOnly=''
 readWrite=''
+remoteHost='192.168.0.10'
 success=''
+username='terryn'
 
 # Get our command line arguments
-while [ "$1" != '' ]
-do
+while [ "$1" != '' ]; do
     case $1 in
         -r | --read-only )      shift
                                 readOnly=1
                                 ;;
         -w | --read-write )     shift
                                 readWrite=1
+                                ;;
+        -n | --dry-run )        shift
+                                dryRun=1
                                 ;;
         -h | --help )           usage
                                 exit 0
@@ -42,25 +51,63 @@ fi
 
 # Set the mounting option
 readOnlyOption=''
-if [[ $readOnly ]]
-then
+if [[ $readOnly ]]; then
     readOnlyOption='-r '
 fi
 
-# Mount the drive
-if mkdir -p /Volumes/share; then
+# Make sure the mount point exists
+mkdirCmd="mkdir -p $mountPoint"
+if [[ $dryRun ]]; then
+    cmd="echo \"Would have run: [$mkdirCmd]\""
+else
+    cmd="$mkdirCmd"
+fi
+
+if eval "$cmd"; then
+    echo "Made directory for mount point."
+
+    # Mount the drive
     echo "Mounting..."
-    if mount $readOnlyOption -t smbfs -o nodev,nosuid smb://terryn:@hs-dhtgl5f5/share /Volumes/share; then
-        success=1
+    mountCmd="mount $readOnlyOption -t smbfs -o nodev,nosuid smb://${username}:@${remoteHost}/share $mountPoint"
+    if [[ $dryRun ]]; then
+	cmd="echo \"Would have run: [$mountCmd]\""
+    else
+	cmd="$mountCmd"
     fi
+    if eval "$cmd"; then
+	success=1
+    fi
+else
+    echo "Make directory command FAILED: [$cmd]."
 fi
 
 if [[ $success ]]
 then       
     echo "Mounted."
-    exit 0
 else
     echo "NOT mounted."
+fi
+
+if [[ $success ]]; then
+    lnCmd="sudo ln -s \"$mountPoint\" \"$linkTo\""
+    if [[ $dryRun ]]; then
+	cmd="echo \"Would have run: [$lnCmd]\""
+    else
+	cmd="$lnCmd"
+    fi
+    if eval "$cmd"; then
+	success=1
+    else
+	success=''
+    fi
+fi
+
+if [[ $success ]]
+then
+    echo "Linked."
+    exit 0
+else
+    echo "NOT linked."
     exit 1
 fi
 
